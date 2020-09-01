67dc79d675a7ee8966982af6d45bf02d82f3d2cd
Max Kalhama <kalhama.max@gmail.com>
2020-09-01 22:08:52 +0300

    Simplify. Just stdout silence json

--- lib/Parser.ts
+++ lib/Parser.ts
@@ -1,117 +1,67 @@
 import { spawn, exec } from 'child_process'
 import * as fs from 'fs-extra' // should I use multer?
+import { strict } from 'assert'
 
 export class Parser {
-  private input: string
-  private output: string
+    private input: string
+    private silence: any[] = []
 
-  private length: number = 0
-  private curr: number = 0
-  private silence: any[] = []
-  private speed: number = 0
-  private copy: boolean = false
-
-  // callbacks
-  private progress?: Function | null
-  private done?: Function | null
-
-  constructor(input: string, output: string, copy: boolean, progress?: Function | null, done?: Function | null) {
-    this.progress = progress
-    this.done = done
-    this.input = input
-    this.output = output
-    this.copy = copy
-  }
-
-  private stderr = (str: string) => {
-    // console.log('stderr: ' + str);
-  
-    this.length <= 0 ? this.setLength(str) : null
-    this.setCurr(str)
-    this.setSpeed(str)
-    this.parseSilence(str)
-
-    if (this.progress && this.length !== 0) {
-      const silenceDelta = this.silence[this.silence.length - 1] || {}
-
-      this.progress(
-        this.length,
-        this.curr,
-        silenceDelta,
-        this.silenceSum(),
-        this.speed
-      )
+    constructor(input: string) {
+        this.input = input
     }
-  }
 
-  private silenceSum = (): number => this.silence.length > 0 ? this.silence.map(obj => obj.duration).reduce((a, b) => a + b) || 0 : 0
-
-  private stdout = (str: string) => console.log('stderr: ' + str)
-
-  public parse = (): void => {
-    spawn('cp', [this.input, `media/${this.output}`]) // TODO a bit dangerous
-    const ffmpeg = spawn('./ffmpeg', ['-i', this.input, '-vn', '-af', 'silencedetect=noise=-30dB:d=0.5', '-f', 'null', '-']);
-    // console.log(ffmpeg)
-  
-    ffmpeg.stdout.on('data', (data: Buffer) => this.stdout(data.toString()));
-    ffmpeg.stderr.on('data', (data: Buffer) => data.toString().split('\n').filter(el => el !== '').forEach(this.stderr))
-  
-    ffmpeg.on('exit', (data: Buffer) => this.exit(data.toString()));
-  }
-
-  private exit = async (str: string) => {
-    await fs.writeFile(`media/${this.output}.js`, 'const silence = ' + JSON.stringify(this.silence, null, 2), 'utf8')
-
-    // TODO add total time took processing and speed
-    if (this.done) {
-      (!this.copy) ? await spawn('rm', [this.input]) : null
-      this.done(str, this.silence, this.length, this.silenceSum())
+    private stderr = (data: Buffer) => {
+        data.toString()
+            .split('\n')
+            .filter((el) => el != '')
+            .forEach((str) => {
+                // console.log('stderr: ' + str);
+                this.parseSilence(str)
+            })
     }
-  }
 
-  private setSpeed = (str: string) => {
-    const match = str.match(/(?<=speed=)(.*)(?=x)/g)
-    match ? this.speed = Number(match[0]) : null
-  }
+    private stdout = (str: Buffer) => console.log('stderr: ' + str.toString())
 
-  private parseSilence = (str: string) => {
-    if (str.includes('silence_start')) {
-      const start = str.match(/(?<=silence_start: )[0-9]+(\.?)[0-9]*/g) || [0]
-      this.silence.push({
-        start: Number(start[0]),
-        end: null,
-        duration: null
-      })
-    } 
-    if (str.includes('silence_end')) {
-      const end = str.match(/(?<=silence_end: )[0-9]+(\.?)[0-9]*/g)
-      const duration = str.match(/(?<=silence_duration: )[0-9]+(\.?)[0-9]*/g)
-      if (end && duration) {
-        const objref = this.silence[this.silence.length -1]
-        objref.end = Number(end[0])
-        objref.duration = Number(duration[0])
-      }
+    public parse = (): void => {
+        const ffmpeg = spawn('ffmpeg', [
+            '-i',
+            this.input,
+            '-vn',
+            '-af',
+            'silencedetect=noise=-30dB:d=0.5',
+            '-f',
+            'null',
+            '-'
+        ])
+
+        ffmpeg.stdout.on('data', this.stdout)
+        ffmpeg.stderr.on('data', this.stderr)
+
+        ffmpeg.on('exit', this.exit)
     }
-  }
 
-  private parseTimestamp = (str: string): number => {
-    // str = "HH:MM:SS:sss"
-    const match = str.match(/[0-9]{2,}/g) //["HH", "MM2, "SS", "sss"]
-    let numArr = match ? match.map(x => Number(x)) : null //[HH, MM, SS, sss] number[]
-    let seconds = numArr ? numArr[0] * 60 * 60 + numArr[1] * 60 + numArr[2] + numArr[3] / 1000 : null // 3600,12 length in seconds
-    if (!seconds) throw new Error(`Invalid input ${str}, could not parse`)
-    return seconds
-  } 
+    private exit = async (_data: Buffer) => {
+        process.stdout.write(JSON.stringify(this.silence))
+        process.exit(0)
+    }
 
-  private setLength = (str: string): void => {
-    let match = str.match(/(?<=Duration: )(.*)(?=, start)/g) //["HH:MM:SS.sss"]
-    const length = match ? this.parseTimestamp(match[0]) : null
-    length ? this.length = length : null
-  }
-
-  private setCurr = (str: string): void => {
-    let match = str.match(/(?<=time=)(.*)(?= bitrate)/g) // //["HH:MM:SS.sss"]
-    const curr = match ? this.parseTimestamp(match[0]) : null
-    curr ? this.curr = curr : null
-  }
+    private parseSilence = (str: string) => {
+        if (str.includes('silence_start')) {
+            const start = str.match(/(?<=silence_start: )[0-9]+(\.?)[0-9]*/g) || [0]
+            this.silence.push({
+                start: Number(start[0]),
+                end: null,
+                duration: null
+            })
+        }
+        if (str.includes('silence_end')) {
+            const end = str.match(/(?<=silence_end: )[0-9]+(\.?)[0-9]*/g)
+            const duration = str.match(/(?<=silence_duration: )[0-9]+(\.?)[0-9]*/g)
+            if (end && duration) {
+                const objref = this.silence[this.silence.length - 1]
+                objref.end = Number(end[0])
+                objref.duration = Number(duration[0])
+            }
+        }
+    }
 }
--- lib/cmd.ts
+++ lib/cmd.ts
@@ -1,40 +1,14 @@
 #!/usr/bin/env node
 
 import * as commander from 'commander'
-import * as cliProgress from 'cli-progress'
 import { Parser } from './Parser'
 
 const program = new commander.Command()
 
 program.version('0.0.1')
 
-program
-    .option('-i, --input <input>', 'input file or url')
-    .option('-o, --output <output>', 'output file')
-    .option('-c, --copy', "Copy video file into app cache instead of move")
-    .parse(process.argv)
+program.option('-i, --input <input>', 'input file or url').parse(process.argv)
 
-if (!program.input) throw new Error("Input is mandatory")
-if (!program.output) program.output = program.input.split('/').pop()
+if (!program.input) throw new Error('Input is mandatory')
 
-const bar = new cliProgress.Bar({
-    format: '{output} [{bar}] {value}% | Speed: {speed}x',
-    etaBuffer: 100
-}, cliProgress.Presets.shades_classic);
-bar.start(100, 0);
-bar.update(0, {speed: '0', output: program.output})
-
-
-
-const done = (exitCode: any, silence: any) => {
-    bar.update(100, {speed: '0'})
-    bar.stop()
-    // console.log(`ffmpeg exit code: ${exitCode}`)
-    process.exit(0)
-}
-
-const progress = (length: any, current: any, silenceDelta: any, silenceSum: any, speed: any) => {
-    bar.update(Math.round(current / length * 10000)/100, {speed: speed.toString()})
-}
-
-new Parser(program.input, program.output, program.copy, progress, done).parse()
+new Parser(program.input).parse()
